ee931c3a64b7d08690a35a969e96192052ec0474
Chotu Kumar <ck4001252@gmail.com>
2025-07-28 13:52:23 +0530

    user routes

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,33 +1,9 @@
-
 import express from "express";
-import User from "../models/userModel";
+import { saveOrUpdateUser } from "../controllers/userController";
+
 
 const router = express.Router();
 
-// Save or update Google user
-router.post("/", async (req, res) => {
-  const { name, email, photo } = req.body;
-
-  if (!email) return res.status(400).json({ message: "Email is required" });
-
-  try {
-    // Check if user already exists
-    let user = await User.findOne({ email });
-
-    if (user) {
-      
-      user.name = name;
-      user.photo = photo;
-      await user.save();
-    } else {
-      user = await User.create({ name, email, photo });
-    }
-
-    res.status(200).json({ message: "User stored", user });
-  } catch (err) {
-    console.error("User saving failed:", err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.post("/", saveOrUpdateUser);
 
 export default router;
